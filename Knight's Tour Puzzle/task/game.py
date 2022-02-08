2ce8ca015ce8e4fe501772921d3c810c45c4b983
RendelFlegg <62382457+RendelFlegg@users.noreply.github.com>
2022-02-08 19:15:32 +0500

    Completed Stage 3: Implemented possible moves

--- Knight's Tour Puzzle/task/game.py
+++ Knight's Tour Puzzle/task/game.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 class KnightGame:
 
     def __init__(self, columns, rows):
@@ -10,6 +13,9 @@ class KnightGame:
         self.border = ' ' * self.row_spaces + '-' * (self.columns * (self.cell_size + 1) + 3)
         self.position = ''
         self.knight = ' ' * (self.cell_size - 1) + 'X'
+        self.move = ' ' * (self.cell_size - 1) + 'O'
+        self.moves = list(itertools.product(['+1', '-1'], ['+2', '-2'])) + \
+                     list(itertools.product(['+2', '-2'], ['+1', '-1']))
 
     def create_board(self):
         for x in range(1, self.columns + 1):
@@ -37,19 +43,39 @@ class KnightGame:
         print(self.border)
         print(' ' * (self.row_spaces + 1), ' '.join(columns_list))
 
+    def check_move(self, coordinates):
+        try:
+            x, y = coordinates.split()
+            x = int(x)
+            y = int(y)
+            assert x in range(1, self.columns + 1) and y in range(1, self.rows + 1)
+        except (ValueError, AssertionError):
+            return False
+        else:
+            return True
+
     def get_start(self):
         while True:
             user_input = input("Enter the knight's starting position: ")
-            try:
+            if self.check_move(user_input):
                 x, y = user_input.split()
-                x = int(x)
-                y = int(y)
-                assert x in range(1, self.columns + 1) and y in range(1, self.rows + 1)
-            except (ValueError, AssertionError):
-                print('Invalid dimensions!')
-            else:
-                self.board['-'.join([str(x), str(y)])] = self.knight
+                self.position = '-'.join([str(x), str(y)])
+                self.board[self.position] = self.knight
                 break
+            else:
+                print('Invalid dimensions!')
+
+    def get_moves(self):
+        moves_list = []
+        knight_x, knight_y = self.position.split('-')
+        for move in self.moves:
+            possible_x = int(knight_x) + int(move[0])
+            possible_y = int(knight_y) + int(move[1])
+            possible_move = f'{possible_x} {possible_y}'
+            if self.check_move(possible_move):
+                moves_list.append(f'{possible_x}-{possible_y}')
+        for move in moves_list:
+            self.board[move] = self.move
 
 
 def get_dimensions():
@@ -71,4 +97,5 @@ user_x, user_y = get_dimensions()
 game = KnightGame(int(user_x), int(user_y))
 game.create_board()
 game.get_start()
+game.get_moves()
 game.print_board()
