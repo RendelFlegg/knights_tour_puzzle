a1c1fb86cdabd7fc14769317475a4bacbce3e4d3
RendelFlegg <62382457+RendelFlegg@users.noreply.github.com>
2022-02-11 12:31:48 +0500

    Completed Stage 5: Game on!

--- Knight's Tour Puzzle/task/game.py
+++ Knight's Tour Puzzle/task/game.py
@@ -11,12 +11,13 @@ class KnightGame:
         self.row_spaces = len(str(rows))
         self.cell_size = len(str(self.columns * self.rows))
         self.border = ' ' * self.row_spaces + '-' * (self.columns * (self.cell_size + 1) + 3)
-        self.position = ''
+        self.position = None
+        self.move_list = ' '
         self.knight = ' ' * (self.cell_size - 1) + 'X'
-        self.move = ' ' * (self.cell_size - 1) + 'O'
+        self.visited = ' ' * (self.cell_size - 1) + '*'
         self.moves = list(itertools.product(['+1', '-1'], ['+2', '-2'])) + \
                      list(itertools.product(['+2', '-2'], ['+1', '-1']))
-        self.visited = []
+        self.visited_cells = []
 
     def create_board(self):
         for x in range(1, self.columns + 1):
@@ -24,7 +25,21 @@ class KnightGame:
                 self.board[f'{x}-{y}'] = '_' * self.cell_size
         return self.board
 
+    def update_board(self):
+        for key in self.board:
+            if key == self.position:
+                self.board[key] = self.knight
+                for move in self.move_list:
+                    clear_moves = [n for n in self.get_moves(move) if n not in self.visited_cells]
+                    move_counter = ' ' * (self.cell_size - 1) + str(len(clear_moves))
+                    self.board[move] = move_counter
+            elif key in self.visited_cells:
+                self.board[key] = self.visited
+            elif key not in self.get_moves(self.position):
+                self.board[key] = '_' * self.cell_size
+
     def print_board(self):
+        self.update_board()
         print(self.border)
         for y in reversed(range(1, self.rows + 1)):
             row_values = []
@@ -33,7 +48,6 @@ class KnightGame:
                 row_number = ' ' * (self.row_spaces - len(row_number)) + row_number
             for x in range(1, self.columns + 1):
                 row_values.append(self.board[f'{x}-{y}'])
-            # print(row_number, '|', ' '.join(row_values), '|')
             print(f'{row_number}|', ' '.join(row_values), '|')
         columns_list = []
         for n in range(1, self.columns + 1):
@@ -44,30 +58,39 @@ class KnightGame:
         print(self.border)
         print(' ' * (self.row_spaces + 1), ' '.join(columns_list))
 
-    def check_move(self, coordinates):
+    def check_move(self, coordinates, player=False):
         try:
             x, y = coordinates.split()
             x = int(x)
             y = int(y)
             assert x in range(1, self.columns + 1) and y in range(1, self.rows + 1)
         except (ValueError, AssertionError):
+            if player:
+                print('Invalid dimensions!')
             return False
         else:
+            if f'{x}-{y}' in self.visited_cells or \
+                    player and self.position and f'{x}-{y}' not in self.get_moves(self.position):
+                if player:
+                    print('Invalid move!', end=' ')
+                return False
             return True
 
-    def get_start(self):
+    def get_move(self):
         while True:
-            user_input = input("Enter the knight's starting position: ")
-            if self.check_move(user_input):
+            if not self.position:
+                user_input = input("Enter the knight's starting position: ")
+            else:
+                user_input = input('Enter your next move: ')
+            if self.check_move(user_input, player=True):
                 x, y = user_input.split()
                 self.position = '-'.join([str(x), str(y)])
+                self.move_list = [n for n in self.get_moves(self.position) if n not in self.visited_cells]
                 self.board[self.position] = self.knight
-                self.visited.append(self.position)
+                self.visited_cells.append(self.position)
                 break
-            else:
-                print('Invalid dimensions!')
 
-    def get_moves_list(self, position):
+    def get_moves(self, position):
         moves_list = []
         knight_x, knight_y = position.split('-')
         for move in self.moves:
@@ -77,12 +100,6 @@ class KnightGame:
             if self.check_move(possible_move):
                 moves_list.append(f'{possible_x}-{possible_y}')
         return moves_list
-
-    def get_moves(self):
-        for move in self.get_moves_list(self.position):
-            clear_moves = [n for n in self.get_moves_list(move) if n not in self.visited]
-            move_counter = ' ' * (self.cell_size - 1) + str(len(clear_moves))
-            self.board[move] = move_counter
 
 
 def get_dimensions():
@@ -103,6 +120,12 @@ user_x, user_y = get_dimensions()
 
 game = KnightGame(int(user_x), int(user_y))
 game.create_board()
-game.get_start()
-game.get_moves()
-game.print_board()
+while game.move_list:
+    game.get_move()
+    game.print_board()
+
+if len(game.board) == len(game.visited_cells):
+    print('What a great tour! Congratulations!')
+else:
+    print('No more possible moves!')
+    print(f'Your knight visited {len(game.visited_cells)} squares!')
